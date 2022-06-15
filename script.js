0fc731f6f2ca2326f69a07e54bdf2d166cc64a7e
Shaishav Shah <shaishav.shah04@gmail.com>
2022-06-15 19:15:05 +0530

    add time units

--- script.js
+++ script.js
@@ -40,6 +40,9 @@ function initMap() {
             draggable: true,
             label,
         });
+        
+        // TimeLimit starts as Null
+        marker.set("timeLimit", null);
 
         // Create card
         createNodeCard(marker);
@@ -62,16 +65,20 @@ function createNodeCard(marker) {
     <p class="mb-0"><b>Lat:</b> ${latlng.lat()}</p>
     <p><b>Lng:</b> ${latlng.lng()}</p>
     <form class="d-flex flex-row" id="timeSelect">
-      <input class="form-control form-control-sm" type="text">
-      <select class="form-select form-select-sm" aria-label="Small select">
-        <option selected value="n">No Time Limit</option>
-        <option value="m">Minutes</option>
-        <option value="h">Hours</option>
-        <option value="d">Days</option>
-      </select>
+        <select class="form-select form-select-sm" aria-label="Small select">
+            <option selected value="n">No Time Limit</option>
+            <option value="min">Minutes</option>
+            <option value="h">Hours</option>
+            <option value="d">Days</option>
+        </select>
+        <input class="form-control form-control-sm" type="number">
     </form>`
 
-    // Actions
+    //-- Actions
+    const dropdownUnits = outermostCard.getElementsByTagName('select')[0];
+    const numericInput = outermostCard.getElementsByTagName('input')[0];
+
+    // Close btn 
     outermostCard.getElementsByClassName('btn-close')[0].addEventListener("click",()=>{
         nodes.filter((e) => {
             if (e == marker) {
@@ -84,35 +91,60 @@ function createNodeCard(marker) {
         document.getElementById("points").removeChild(outermostCard);
     }); 
 
-    // let title = document.createElement("h5");
-    // title.innerText = "Point " + marker.getLabel();
-    // let btn = document.createElement("button");
-    // btn.classList.add('btn-close')
-    // btn.type="button";
-    // btn.addEventListener("click",()=>{
-    //     nodes.filter((e) => {
-    //         if (e == marker) {
-    //             marker.setMap(null);
-    //             return false;
-    //         } else {
-    //             return true;
-    //         }
-    //     })
-    //     document.getElementById("points").removeChild(outermostCard);
-    // }); 
-    // let div = document.createElement("div");
-    // div.classList.add("d-flex","flex-row","justify-content-between");
-    // div.appendChild(title);
-    // div.appendChild(btn);
-    // outermostCard.appendChild(div);
-    // let p = document.createElement("p");
-    // p.classList.add("mb-0");
-    // let latlng = marker.getPosition();
-    // p.innerHTML = `<b>Lat: </b> ${latlng.lat()}`;
-    // outermostCard.appendChild(p);
-    // p = document.createElement("p");
-    // p.innerHTML = `<b>Lng: </b> ${latlng.lng()}`;
-    // outermostCard.appendChild(p);
+    // Form Submit
+    outermostCard.getElementsByTagName("form")[0].addEventListener("submit", function(event){
+        // Prevent Refresh
+        event.preventDefault();
+        // Get Values
+        let timeUnits = dropdownUnits.value;
+        let timeValue = parseInt(numericInput.value);
+        // Checks
+        if (timeUnits == "n") {
+            marker.set("timeLimit", null);
+            alert("Input a value or remove time-constraint!");
+            return; 
+        }
+
+        if (timeValue == 0) {
+            numericInput.value = "";
+            numericInput.style.display = "none";
+            marker.set("timeLimit", null);
+            return;
+        }
+
+        if (timeValue < 0) {
+            alert("Don't use negative time values! Thats not cool!");
+            return;
+        }
+
+        let minutesValue;
+        
+        switch(timeUnits) {
+            case "min":
+                minutesValue = timeValue;
+                break;
+            case "h":
+                minutesValue = timeValue * 60;
+                break;
+            case "d":
+                minutesValue = timeValue * 60 * 24;
+                break;
+        }
+        marker.set("timeLimit", minutesValue);
+    });
+
+    // DropDown causes input to disappear
+    dropdownUnits.addEventListener("change", function(){
+        if (this.value == "n") {
+            numericInput.value = "";
+            numericInput.style.display = "none";
+            marker.set("timeLimit", null);
+        } else {
+            numericInput.style.display = "block";
+        }
+    })
+
+    
     document.getElementById("points").appendChild(outermostCard);
 }
 
